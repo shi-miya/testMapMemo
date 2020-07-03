74a2c087707cc5e889590c49475fd55790ca40f0
shi-miya <colon9anje9@icloud.com>
2020-07-04 07:18:00 +0900

    Initial Commit

---
+++ testMapMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testMapMemo
+//
+//  Created by 宮本翼 on 2020/07/04.
+//  Copyright © 2020 宮本翼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
